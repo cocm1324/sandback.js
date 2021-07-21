055678ecc8ab9ca29833882833d35498bc35d3d3
cocm1324 <cocm1324@gmail.com>
2021-07-21 17:27:02 +0900

    UPDATE: missing changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export * from '@src/decorators';
+export * from './decorators';
+export * from './errors';
+export * from './middlewares';
+
+export function startServer(SandbagApp: any): Promise<any> {
+    const app = new SandbagApp();
+
+    if (!app || !app['start']) {
+        throw 'Invalid Sandbag App';
+    }
+
+    const pending: Promise<string> = app['start']();
+    return pending;
+}
